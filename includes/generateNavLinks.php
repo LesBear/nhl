73ffe531cf90e6e057a779d2201a865e67ff1ee9
perskjans <erskjans.per@gmail.com>
2016-05-16 15:15:51 +0200

    Added player search for first and last name.

--- includes/generateNavLinks.php
+++ includes/generateNavLinks.php
@@ -28,4 +28,32 @@ function generateNavLinks($pageNum, $count, $numRowsToShow, $siteToLink, $search
     return $links;
 }
 
+
+function generateNavLinksPlayerSearch($pageNum, $count, $numRowsToShow, $siteToLink, $firstName, $lastName){
+    
+	$back = $pageNum - 1;
+	$forward = -1;
+	
+	if($pageNum < (int)($count / $numRowsToShow)){
+		$forward = $pageNum + 1;			
+	}
+	
+	$startTag = "<a href='".$siteToLink."?firstname=" .$firstName ."&lastname=" .$lastName ."&page=";
+    $closeStartTag = "'>";
+    $endTag = "</a>";
+
+    $links = "";
+
+    if ($back !== -1){
+        $links .= $startTag .$back .$closeStartTag ."Prev" .$endTag;
+    }
+	
+	$links .= "  Page " .($pageNum + 1) ." of " .((int)($count / $numRowsToShow) + 1) ."  ";
+	
+    if ($forward !== -1){
+        $links .= $startTag .$forward .$closeStartTag ."Next" .$endTag;
+    }
+
+    return $links;
+}
 ?>
---
+++ includes/player.php
@@ -0,0 +1,63 @@
+<?php
+
+function findPlayer($name, $lastName, $page=0){
+	
+	$myDBconnection = db_connect();
+	$playerQuery = "SELECT
+		pt.player_number AS '#',
+		p.first_name AS 'Name',
+		p.last_name AS 'Last Name',
+		pt.player_end_team AS 'Team',
+		ps.GP AS 'GP',
+		ps.A AS 'A',
+		ps.A1 AS 'A1',
+		ps.PTS AS 'PTS',
+		ps.plus_minus AS '+/-',
+		p.id_player AS 'ID'
+		
+    FROM nhl.players p
+
+	INNER JOIN nhl.player_team pt
+	ON p.id_player = pt.id_player 
+	INNER JOIN nhl.player_stats ps
+	ON p.id_player = ps.id_player
+	
+	WHERE p.first_name LIKE '%".$name."%' OR p.last_name LIKE '%".$lastName."%' 
+	ORDER BY p.last_name ASC, p.first_name ASC
+	LIMIT ".($page*10).", 10;";
+
+	$result = mysqli_query($myDBconnection ,$playerQuery);
+	
+	if ($result){
+		if(mysqli_num_rows($result)){
+			$i = 0;
+			$data = array();
+			while($row = mysqli_fetch_assoc($result)){
+				$data[$i] = $row;
+				$i++;
+			}
+		
+			return $data;
+		}
+	} else {
+		return "empty result";
+	}
+}
+
+
+function countPlayers($name, $lastName){
+
+	$myDBconnection = db_connect();
+	$countQuery = "SELECT
+		COUNT(p.first_name)
+		
+    FROM nhl.players p
+	
+	WHERE p.first_name LIKE '%".$name."%' OR p.last_name LIKE '%".$lastName."%';";
+
+	$result = mysqli_query($myDBconnection ,$countQuery);
+	$result = mysqli_fetch_row($result);
+
+	return $result[0];
+}
+?>
